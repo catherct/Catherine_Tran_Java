7ccbfe8180e156d39f3e3cd0c658031ad53d5155
Catherine Tran <cathy_tran@CatherinesAir2.attlocal.net>
2022-10-25 16:40:25 -0700

    deleted methods

--- M9-Challenge-Tran-Catherine/module-9-challenge/src/main/java/com/company/module9challenge/repository/CustomerRepository.java
+++ M9-Challenge-Tran-Catherine/module-9-challenge/src/main/java/com/company/module9challenge/repository/CustomerRepository.java
@@ -9,12 +9,6 @@ import java.util.List;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
-    List<Customer> createCustomer(Customer customer);
-
-    List<Customer> updateCustomer(Customer customer);
-
-    List<Customer> deleteCustomer();
-
     List<Customer> findCustomerById(int customerId);
 
     List<Customer> findCustomersByState(String state);
